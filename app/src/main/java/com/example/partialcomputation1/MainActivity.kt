6e20588cd17495d3d102d5293832d72955c7b599
Daniel Castillo <ec1335784@gmail.com>
2022-02-26 19:05:50 -0600

    Connect Code to the Desing

--- app/src/main/java/com/example/partialcomputation1/MainActivity.kt
+++ app/src/main/java/com/example/partialcomputation1/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.partialcomputation1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txtNumberoOne = findViewById<EditText>(R.id.txtNumeroOne)
+        val txtNumberoTwo = findViewById<EditText>(R.id.txtNumeroTwo)
+        val btnSumar = findViewById<Button>(R.id.btnSumar)
+        val txtResultado = findViewById<TextView>(R.id.txtResultado)
+        var suma :Int
     }
 }
