c969d6e63def30f793133d64ff3cc87658e9df48
Cesar Martinez <oliver-cesar@hotmail.com>
2022-02-27 11:28:12 -0600

    Add Function Button Sum

--- app/src/main/java/com/example/partialcomputation1/MainActivity.kt
+++ app/src/main/java/com/example/partialcomputation1/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.partialcomputation1
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,6 +9,7 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +19,20 @@ class MainActivity : AppCompatActivity() {
         val btnSumar = findViewById<Button>(R.id.btnSumar)
         val txtResultado = findViewById<TextView>(R.id.txtResultado)
         var suma :Int
+
+
+        btnSumar.setOnClickListener {
+
+            if (txtNumberoOne.text.isNotEmpty() && txtNumberoTwo.text.isNotEmpty()) {
+                suma = (txtNumberoOne.text.toString()).toInt() + (txtNumberoTwo.text.toString()).toInt()
+                txtResultado.text = "The result is: $suma"
+                txtResultado.setTextColor(Color.parseColor("#008000"))
+            }else {
+                if (txtNumberoOne.text.isNotEmpty() && txtNumberoTwo.text.isEmpty()) {
+                    txtResultado.text = "El Resultado es: ${txtNumberoOne.text}"
+                    txtResultado.setTextColor(Color.parseColor("#FF8000"))
+                }
+            }
+        }
     }
 }
